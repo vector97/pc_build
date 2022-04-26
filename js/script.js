37a60e30963f533c828d6dd5c8b7d4b266071184
Viktor Bogatenkov <vector_97@mail.ru>
2022-04-26 16:50:35 +0400

    Реализовано переключение табов

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const contents = document.querySelectorAll(".program-line__content");
+
+contents.forEach((elem) => {
+  const title = elem.querySelector(".program-line__title");
+
+
+  title.addEventListener("click", () => {
+    contents.forEach((el) => {
+      const descr = el.querySelector(".program-line__descr");
+      if (elem === el) {
+        descr.classList.toggle("active");
+      } else {
+        descr.classList.remove("active");
+      }
+    });
+  });
+});
